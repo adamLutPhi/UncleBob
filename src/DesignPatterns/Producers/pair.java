2067ccafbd6972f4fbd0861afcbf8aac472f0377
Ahmad Lutfi <lutfipro91@gmail.com>
2023-10-02 20:36:01 -0400

    PR: add`pair`

---
+++ src/DesignPatterns/Producers/pair.java
@@ -0,0 +1,13 @@
+package DesignPatterns.Producers;
+
+public class pair {
+    public Integer id;
+    public String name;
+
+    pair(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+}
